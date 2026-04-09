4615cb766b2c1afd7b3694f13b1b070e680713e3
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-09 20:39:32 +0530

    Master lambda expression

---
+++ Lambda-Expression/src/Window/MyWindow.java
@@ -0,0 +1,33 @@
+package Window;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWindow {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My Window");
+        frame.setSize(400, 300);
+        frame.setLayout(new FlowLayout());
+
+        // Create click Here button and add on window
+        JButton button = new JButton("Click Here");
+        frame.add(button);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Button is clicked");
+                JOptionPane.showMessageDialog(null, "Button is clicked!!");
+            }
+        });
+
+    }
+
+
+
+}
