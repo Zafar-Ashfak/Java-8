257eec7ab624029ef336d30e2a99b512f720a5a1
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-09 20:17:13 +0530

    Master lambda expression

--- Lambda-Expression/src/ThreadDemo.java
+++ Lambda-Expression/src/ThreadDemo.java
@@ -1,11 +1,12 @@
 public class ThreadDemo {
     public static void main(String[] args) {
 
+        // Print number from 1 to 10 using Runnable Class
         Runnable thread1 = () -> {
             for (int i = 1; i <= 10; i++) {
                 System.out.print(i + " ");
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(700);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -13,7 +14,26 @@ public class ThreadDemo {
 
         };
 
-        thread1.run();
+//        thread1.run();
+//        System.out.println();
 
+        // Print even number using Runnable Class
+        Runnable thread2 = () -> {
+            for (int i = 1; i <= 50; i++) {
+               if (i % 2 == 0) {
+                   System.out.print(i + " ");
+               }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+
+        };
+
+        thread2.run();
     }
 }
