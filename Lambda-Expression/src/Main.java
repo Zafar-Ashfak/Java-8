922a70718044b425671ac4fa3e49e84beda7a029
zafaraftab1 <zafaraftab14581@gmail.com>
2026-04-09 13:40:45 +0530

    Master lambda expression

--- Lambda-Expression/src/Main.java
+++ Lambda-Expression/src/Main.java
@@ -3,27 +3,37 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Way 1. Create a separate class and implements the interface
-        InterfaceImpl impl = new InterfaceImpl();
-        impl.greet();
+//        // Way 1. Create a separate class and implements the interface
+//        InterfaceImpl impl = new InterfaceImpl();
+//        impl.greet();
+//
+//
+//        // Way 2. Create an anonymous class for implementing interface
+//        MyInterface m1 = new InterfaceImpl() {
+//            public void greet() {
+//                System.out.println("Hello from anonymous class 1");
+//            }
+//        };
+//
+//        m1.greet();
+//
+//       MyInterface m2 = new InterfaceImpl() {
+//           public void greet() {
+//               System.out.println("Hello from anonymous class 2");
+//           }
+//       };
+//
+//       m2.greet();
 
-
-        // Way 2. Create an anonymous class for implementing interface
-        MyInterface m1 = new InterfaceImpl() {
-            public void greet() {
-                System.out.println("Hello from anonymous class 1");
-            }
+       // Way 3. Lambda expression for implementing interface
+        MyInterface l1 = () -> {
+            System.out.println("Hello from lambda implementation");
         };
 
-        m1.greet();
+        l1.greet();
 
-       MyInterface m2 = new InterfaceImpl() {
-           public void greet() {
-               System.out.println("Hello from anonymous class 2");
-           }
-       };
 
-       m2.greet();
-
+        MyInterface l2 = () -> System.out.println("Hello from lambda implementation second time.");
+        l2.greet();
     }
 }
