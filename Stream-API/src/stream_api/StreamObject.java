0a18dcdf8c286769ae5241df51638b39d1fb39cf
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-14 21:28:24 +0530

    Practice Java 8 features

--- Stream-API/src/stream_api/StreamObject.java
+++ Stream-API/src/stream_api/StreamObject.java
@@ -6,19 +6,19 @@ import java.util.stream.Stream;
 
 public class StreamObject {
     public static void main(String[] args) {
-        // 1.
+        // 1. stream empty method
        Stream<Object> emptyStream = Stream.empty();
         System.out.println(emptyStream);
 
-        // 2.
+        // 2. filter string array using stream api
        String[] names = {"Ashfaq",  "Sajid", "Sachin", "Atul", "Sams", "Sahil"};
        Stream<String> filteredNames = Stream.of(names);
        filteredNames.forEach(System.out::println);
 
-       // 3.
+       // 3. string builder
         Stream<Object> streamBuilder = Stream.builder().build();
 
-        // 4.
+        // 4. print numbers using IntStream API
         IntStream stream = Arrays.stream(new int[]{2, 12, 5, 6});
         stream.forEach(System.out::println);
     }
