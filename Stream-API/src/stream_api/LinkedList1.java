48665b228620d925254f129d7b8088665b1e1cce
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-16 22:11:36 +0530

    Practice Java 8 features

--- Stream-API/src/stream_api/LinkedList1.java
+++ Stream-API/src/stream_api/LinkedList1.java
@@ -2,8 +2,8 @@ package LinkedList;
 
 public class LinkedList1 {
     public static class Node {
-        int data;
-        Node next;
+        public int data;
+        public Node next;
 
         public Node(int data) {
             this.data = data;
