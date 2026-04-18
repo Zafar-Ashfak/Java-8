96150d3485e1632a096c7ee9086d941769dd47e6
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-18 14:50:21 +0530

    Practice Java 8 features

--- Exception Handling/src/Main.java
+++ Exception Handling/src/Main.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -12,19 +15,29 @@ public class Main {
             System.out.println(res);
             System.out.println("Done");
         } catch (Exception e) {
-            System.out.println("Exception occurred");
-
+            System.out.println("Arithmetic Exception");
+        } finally {
+            System.out.println("Finally always execute");
         }
+
+
 
 
         int[] arr = {2, 6, 9, 14};
         try {
             System.out.println(arr[5]);
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("Exception");
+            System.out.println("Index out of bound Exception");
         }
 
         System.out.println("Outside try-catch block");
+
+
+        try {
+            FileReader file = new FileReader("zafar_ashfak.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("Exception in file reader");
+        }
 
     }
 }
