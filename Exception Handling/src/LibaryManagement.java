0346f513b07ebd46b35f33730cb730666bafeffc
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-18 15:23:08 +0530

    Practice Java 8 features

---
+++ Exception Handling/src/LibaryManagement.java
@@ -0,0 +1,35 @@
+class Libary {
+    int[] books = { 101, 102, 103, 104, 105};
+
+    public void borrowBook(int bookId) {
+        try {
+            boolean found = false;
+            for (Integer book : books) {
+                if (book == bookId) {
+                    System.out.println("Book Request: " + book);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                System.out.println("Book with Id: " + bookId + " not found");
+            }
+
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception :: ArrayIndexOutOfBoundsException Occurred");
+        } catch (NullPointerException n) {
+            System.out.println("Exception :: NullPointerException Occurred");
+        } finally {
+            System.out.println("Finally executes");
+        }
+    }
+}
+
+public class LibaryManagement {
+    public static void main(String[] args) {
+        Libary libary = new Libary();
+        libary.borrowBook(161);
+    }
+}
