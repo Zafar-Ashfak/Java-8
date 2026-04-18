be44aabdc736fdfee11f9b303c09cfb6982164c9
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-18 13:37:25 +0530

    Practice Java 8 features

---
+++ Exception Handling/src/Main.java
@@ -0,0 +1,30 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        int num = 10;
+//        int res = num / 0;
+//        System.out.println(res);
+//        System.out.println("Done");
+        try {
+            int num = 10;
+            int res = num / 0;
+            System.out.println(res);
+            System.out.println("Done");
+        } catch (Exception e) {
+            System.out.println("Exception occurred");
+
+        }
+
+
+        int[] arr = {2, 6, 9, 14};
+        try {
+            System.out.println(arr[9]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Outside try-catch block");
+
+    }
+}
