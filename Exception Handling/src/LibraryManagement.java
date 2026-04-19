eddf1ae0554e2477a50263cd3765009ece2f5348
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-19 11:48:27 +0530

    Practice Java 8 features

--- Exception Handling/src/LibraryManagement.java
+++ Exception Handling/src/LibraryManagement.java
@@ -1,35 +1,47 @@
 class Library {
     int[] books = { 101, 102, 103, 104, 105};
 
-    public void borrowBook(int bookId) {
-        try {
-            boolean found = false;
-            for (Integer book : books) {
-                if (book == bookId) {
-                    System.out.println("Book Request: " + book);
-                    found = true;
-                    break;
-                }
+//    public void borrowBook(int bookId) {
+//        try {
+//            boolean found = false;
+//            for (Integer book : books) {
+//                if (book == bookId) {
+//                    System.out.println("Book Request: " + book);
+//                    found = true;
+//                    break;
+//                }
+//            }
+//
+//            if (!found) {
+//                System.out.println("Book with Id: " + bookId + " not found");
+//            }
+//
+//
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("Exception :: ArrayIndexOutOfBoundsException Occurred");
+//        } catch (NullPointerException n) {
+//            System.out.println("Exception :: NullPointerException Occurred");
+//        } finally {
+//            System.out.println("Finally executes");
+//        }
+//    }
+
+    public void borrowBook(int bookRequestedCount) throws Exception {
+            int availableBooks = 3;
+            if (bookRequestedCount > availableBooks) {
+                throw new Exception("Book not found");
             }
-
-            if (!found) {
-                System.out.println("Book with Id: " + bookId + " not found");
-            }
-
-
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Exception :: ArrayIndexOutOfBoundsException Occurred");
-        } catch (NullPointerException n) {
-            System.out.println("Exception :: NullPointerException Occurred");
-        } finally {
-            System.out.println("Finally executes");
-        }
     }
+
 }
 
 public class LibraryManagement {
     public static void main(String[] args) {
         Library library = new Library();
-        library.borrowBook(161);
+        try {
+            library.borrowBook(161);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- Exception Handling/src/Main.java
+++ Exception Handling/src/Main.java
@@ -3,39 +3,22 @@ import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) {
-//        int num = 10;
-//        int res = num / 0;
-//        System.out.println(res);
-//        System.out.println("Done");
-        try {
-            int num = 10;
-            int res = num / 0;
-            System.out.println(res);
-            System.out.println("Done");
-        } catch (Exception e) {
-            System.out.println("Arithmetic Exception");
-        } finally {
-            System.out.println("Finally always execute");
-        }
-
-
-
-
-        int[] arr = {2, 6, 9, 14};
-        try {
-            System.out.println(arr[5]);
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Index out of bound Exception");
-        }
-
-        System.out.println("Outside try-catch block");
-
+        int[] nums = {1, 2, 3, 4, 5};
+        System.out.println("Hello Devs");
 
         try {
-            FileReader file = new FileReader("zafar_ashfak.txt");
-        } catch (FileNotFoundException e) {
-            System.out.println("Exception in file reader");
+            int res = 10/ 0;
+            System.out.println(nums[8]);
+
+        } catch (ArithmeticException e) {
+            System.out.println(e.getStackTrace());
+            System.out.println(e.getMessage());
+            System.out.println(e);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Tried to access an element that exceeded the length of the array");
         }
 
+        System.out.println("Bye Devs");
     }
 }
