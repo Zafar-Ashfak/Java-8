348fdbc93495a4b939eaa4a3b5db44be1b1e97c5
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-19 18:05:44 +0530

    Practice Java 8 featues

---
+++ GenericAndWrapperClass/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        Integer obj1 = new Integer(121);
+        Integer obj1 = Integer.valueOf(121);
+
+        int num = obj1;
+        System.out.println(num);
+
+        Boolean check  = Boolean.valueOf("false");
+        boolean res = check;
+        System.out.println(res);
+
+
+    }
+}
