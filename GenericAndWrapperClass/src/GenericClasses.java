c508c4c5ddefd4ca39623b455864dcc285774318
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-20 16:54:14 +0530

    Practice Java 8 features

--- GenericAndWrapperClass/src/GenericClasses.java
+++ GenericAndWrapperClass/src/GenericClasses.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
-
 public class GenericClasses {
+    public static void main(String[] args) {
+        Fish<String> tuna = new Fish<>("tuna1211");
+        System.out.println(tuna.getId());
+
+        Fish<String> shark = new Fish<>("Shark98098");
+        System.out.println(shark.getId());
+
+        Fish<Integer> whale = new Fish<>(900);
+        System.out.println(whale.getId());
+
+// ****************************************************************************
+        System.out.println("_________________________________________________");
+        Dog<String, String> d1 = new Dog<>("Tommy3295", "Tommy");
+        System.out.println("Dog Id: " + d1.getId() + ", Name: " +  d1.getName());
+
+        Dog<String, String> d2 = new Dog<>("Bob2144", "Bob");
+        System.out.println("Dog Id: " + d2.getId() + ", Name: " +  d2.getName());
+
+        Dog<Integer, String> d3 = new Dog<>(134, "Jacky");
+        System.out.println("Dog Id: " + d3.getId() + ", Name: " +  d3.getName());
+
+    }
+}
+
+class Fish<E> {
+    E id;
+
+    public Fish(E id) {
+        this.id = id;
+    }
+
+    E getId() {
+        return id;
+    }
+}
+
+
+class Dog<E, V> {
+    E id;
+    V name;
+
+    public Dog(E id, V name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    E getId() {
+        return id;
+    }
+
+    V getName() {
+        return name;
+    }
 }
--- GenericAndWrapperClass/src/GenericMethods.java
+++ GenericAndWrapperClass/src/GenericMethods.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class GenericMethods {
+
+    // This static generic method can print any type of data
+    public static <E> void printData(E data) {
+        System.out.println(data);
+    }
+
+    // This generic method can print any type of data
+    public <E> void display(E data) {
+        System.out.println(data);
+    }
+
+    // This generic method can only print number such as Integer or Decimal
+    public <T extends Number> void print(T num) {
+        System.out.println(num);
+    }
+
+    public static void main(String[] args) {
+        printData("I don't know why!");
+        printData(620305);
+        printData(7480.54);
+
+
+        GenericMethods obj = new GenericMethods();
+        obj.display("but I am upset");
+        obj.display(2154.903);
+        obj.display(24032026);
+
+
+        GenericMethods obj2 = new GenericMethods();
+//        obj2.print("String can be print if we don't bound it.");
+        obj2.print(1241);
+        obj2.print(503471.322);
+    }
+
 }
+
