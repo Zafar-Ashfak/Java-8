4fbd320201158de6456dbbaddba55280e88297c3
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-22 16:28:05 +0530

    Practice multi threading concepts

--- Multi-Threading/src/Main.java
+++ Multi-Threading/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     static void main() {
         // Run the thread using Runnable interface
-//        MyRunnable t1 = new MyRunnable();
-//        Thread thr = new Thread(t1);
-//        thr.start();
+        MyRunnable t1 = new MyRunnable();
+        Thread thr = new Thread(t1);
+        thr.start();
 
         // Run the thread using Thread class
         MyThread t2 = new MyThread();
--- Multi-Threading/src/threadOp.java
+++ Multi-Threading/src/threadOp.java
@@ -1,21 +1,23 @@
 // Operations on thread
 public class threadOp {
     static void main() {
-        System.out.println("Program is started...");
+        System.out.println("Program started...");
 
-        int num1 = 20;
-        int num2 = 30;
-        System.out.println(num1 + num2);
+        int num1 = 100;
+        int num2 = 50;
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
 
-        // Operations
         Thread thr = Thread.currentThread();
-        String threadName =  thr.getName();
-        System.out.println("Current running thread is: " + threadName);
-        // Setting the name of the current thread
-        thr.setName("myMainMethod");
-        System.out.println("Now current running thread is: " + thr.getName());
+        System.out.println("Name of the current thread is: " + thr.getName());
 
+        thr.setName("master");
+        System.out.println("Now the new name of the current thread is: " + thr.getName());
+        System.out.println("Thread Id is: " + thr.threadId());
+        System.out.println("Thread group is: " + thr.getThreadGroup());
+        System.out.println("Thread context class loader is: " + thr.getContextClassLoader());
+        System.out.println("Thread context priority is: " + thr.getPriority());
 
-        System.out.println("Program is ended......");
+        System.out.println("Program ended...");
+
     }
 }
