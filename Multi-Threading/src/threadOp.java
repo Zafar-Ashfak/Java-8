04e333e7bace64941407d5f63e86b4ba6bddf9cc
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-22 16:44:46 +0530

    Practice multi threading concepts

--- Multi-Threading/src/threadOp.java
+++ Multi-Threading/src/threadOp.java
@@ -1,6 +1,14 @@
 // Operations on thread
+
+
+class UserThread implements Runnable {
+    public  void run() {
+        System.out.println("This is user defined thread");
+    }
+}
+
 public class threadOp {
-    static void main() {
+    public static void main() throws InterruptedException {
         System.out.println("Program started...");
 
         int num1 = 100;
@@ -16,6 +24,19 @@ public class threadOp {
         System.out.println("Thread group is: " + thr.getThreadGroup());
         System.out.println("Thread context class loader is: " + thr.getContextClassLoader());
         System.out.println("Thread context priority is: " + thr.getPriority());
+
+        System.out.println("\n____________________________________________");
+
+        try {
+            System.out.println("\nUser thread is running...");
+            UserThread userThr = new UserThread();
+            Thread th = new Thread(userThr);
+            th.start();
+
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
         System.out.println("Program ended...");
 
