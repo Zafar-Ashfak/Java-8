81bae7af6a1a5913f9afc2f58e236a295bb54918
Zafar-Ashfak <Zafarashfak008@gmail.com>
2026-04-24 17:59:55 +0530

    Practice Stream API

---
+++ Stream-API/src/stream_api/StreamMethods.java
@@ -0,0 +1,20 @@
+package stream_api;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamMethods {
+    public static void main(String[] args) {
+        List<Integer> list1 =  List.of(1, 5, 3, 2, 13, 6, 9, 4, 15, 11, 8);
+
+        // ----->>>>>>>>>> Methods <<<<<<<<<<-------
+        // 1. Filter Method
+        List<Integer> filteredList = list1.stream().filter(e -> e > 3 && e < 13).toList();
+
+        System.out.println(filteredList);
+
+
+
+
+    }
+}
